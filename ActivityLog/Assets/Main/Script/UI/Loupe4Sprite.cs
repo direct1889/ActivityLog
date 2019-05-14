71c45ca73fefb8c87195b773f22ee7454a695a07
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-05-14 19:00:54 +0900

    Loupe4Sprite
    Sprite2Dをルーペに。UIより座標移動が軽い(はず)

---
+++ ActivityLog/Assets/Main/Script/UI/Loupe4Sprite.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UGUI = UnityEngine.UI;
+
+namespace Main.UI {
+
+    // 使用時はCanvasのRenderModeをCameraにすること
+    public class Loupe4Sprite : MonoBehaviour {
+
+        Material m_mat = null;
+        RectTransform m_recT = null;
+        RectTransform m_parentRecT = null;
+
+        void Awake() {
+            m_mat = GetComponent<SpriteRenderer>().material;
+            //m_mat = GetComponent<UGUI.Image>().material;
+            m_recT = GetComponentInParent<RectTransform>();
+            m_parentRecT = transform.parent.GetComponentInParent<RectTransform>();
+        }
+
+        void Update() {
+            //var uv = m_recT.position / m_parentRecT.rect.size;
+            var posOnScreen = Camera.main.WorldToScreenPoint(transform.position);
+            m_mat.SetFloat("_UVx", posOnScreen.x / Screen.width);
+            m_mat.SetFloat("_UVy", 1f - posOnScreen.y / Screen.height);  // UI座標はyが上から
+        }
+    }
+
+}
---
+++ ActivityLog/Assets/Main/Script/UI/Loupe4UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UGUI = UnityEngine.UI;
+
+namespace Main.UI {
+
+    // 使用時はCanvasのRenderModeをOverlayにすること
+    public class Loupe4UI : MonoBehaviour {
+
+        Material m_mat = null;
+        RectTransform m_recT = null;
+        RectTransform m_parentRecT = null;
+
+        void Awake() {
+            m_mat = GetComponent<UGUI.Image>().material;
+            m_recT = GetComponentInParent<RectTransform>();
+            m_parentRecT = transform.parent.GetComponentInParent<RectTransform>();
+        }
+
+        void Update() {
+            var uv = m_recT.position / m_parentRecT.rect.size;
+            m_mat.SetFloat("_UVx", uv.x);
+            m_mat.SetFloat("_UVy", 1f - uv.y);  // UI座標はyが上から
+        }
+    }
+
+}
