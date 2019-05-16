4e60968e70e8fc88acb1ba0d4373be5c08d63dd5
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-05-17 01:04:37 +0900

    シリンダ用にOutlineシェーダ改良、ShaderAttachments始動

---
+++ ActivityLog/Assets/Main/Script/UI/ShaderAttachment/Outline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UGUI = UnityEngine.UI;
+
+namespace ShaderAttachment {
+
+    public class Outline : MonoBehaviour {
+        Material m_mat = null;
+        RectTransform m_recT = null;
+        void Awake() {
+            m_mat = GetComponent<UGUI.Image>().material;
+            m_recT = GetComponent<RectTransform>();
+            m_mat.SetFloat("_ObjSizeX", m_recT.rect.width);
+            m_mat.SetFloat("_ObjSizeY", m_recT.rect.height);
+        }
+    }
+
+}
