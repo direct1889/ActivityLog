9e95791138d93aa7761fc4323a3ea1a4601bd8fe
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-06-02 11:50:47 +0900

    ボタンでDoActivityパネルを表示

---
+++ ActivityLog/Assets/Main/Script/MainMgr.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace Main {
+
+    public class MainMgr : MonoBehaviour {
+        #region field
+        [SerializeField] GameObject m_graphCanvas = null;
+        [SerializeField] GameObject m_doActCanvas = null;
+        #endregion
+
+        #region mono
+        private void Start() {
+            du.dui.RxButtonMgr.OnClickAsObservable("DoActivity")
+                .Subscribe(_ => m_doActCanvas.SetActive(!m_doActCanvas.activeSelf))
+                .AddTo(this);
+        }
+        #endregion
+    }
+
+}
