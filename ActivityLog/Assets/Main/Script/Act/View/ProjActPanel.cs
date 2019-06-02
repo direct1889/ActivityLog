3d2450960aeca313bcc5b26ae7d7846ead6d13e7
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-06-03 01:56:00 +0900

    tab -> 4spaces

--- ActivityLog/Assets/Main/Script/Act/View/ProjActPanel.cs
+++ ActivityLog/Assets/Main/Script/Act/View/ProjActPanel.cs
@@ -4,40 +4,40 @@ using System.Collections.Generic;
 
 namespace Main.Act.View {
 
-	public interface IROProjActPanel {
-	}
+    public interface IROProjActPanel {
+    }
 
-	public interface IProjActPanel : IROProjActPanel {
-	}
+    public interface IProjActPanel : IROProjActPanel {
+    }
 
-	public class ProjActPanel : MonoBehaviour, IProjActPanel {
-		#region field
-		IProject m_proj = null;
+    public class ProjActPanel : MonoBehaviour, IProjActPanel {
+        #region field
+        IProject m_proj = null;
 
-		UImage m_frame = null;
-		[SerializeField] TMPro.TMP_Text m_name = null;
-		[SerializeField] GameObject m_effectiveCert = null;
-		#endregion
+        UImage m_frame = null;
+        [SerializeField] TMPro.TMP_Text m_name = null;
+        [SerializeField] GameObject m_effectiveCert = null;
+        #endregion
 
-		#region getter
-		#endregion
+        #region getter
+        #endregion
 
-		#region mono
-		private void Awake() {
-			m_frame = GetComponent<UImage>();
-		}
-		#endregion
+        #region mono
+        private void Awake() {
+            m_frame = GetComponent<UImage>();
+        }
+        #endregion
 
-		#region public
-		private void Initialize(IProject proj) {
-			if (m_proj == null) {
-				m_proj = proj;
-				m_name.text = proj.Name;
-				m_frame.material.color = m_proj.Color;
-				m_effectiveCert.SetActive(m_proj.IsEffectiveDefault);
-			}
-		}
-		#endregion
-	}
+        #region public
+        private void Initialize(IProject proj) {
+            if (m_proj == null) {
+                m_proj = proj;
+                m_name.text = proj.Name;
+                m_frame.material.color = m_proj.Color;
+                m_effectiveCert.SetActive(m_proj.IsEffectiveDefault);
+            }
+        }
+        #endregion
+    }
 
 }
