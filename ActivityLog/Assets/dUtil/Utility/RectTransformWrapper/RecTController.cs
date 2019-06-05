467ccddd3ac06ca24bd68e3ec3b443ae562ac891
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-06-05 17:55:38 +0900

    RecTControllerに右上原点を追加

--- ActivityLog/Assets/dUtil/Utility/RectTransformWrapper/RecTController.cs
+++ ActivityLog/Assets/dUtil/Utility/RectTransformWrapper/RecTController.cs
@@ -21,8 +21,11 @@ namespace du.Cmp.RecT {
 
         protected RecTController(RectTransform rect) { RecT = rect; }
 
+        public override string ToString() { return $"Offset[Min({OffmX}, {OffmY}), Max({OffMX}, {OffMY})]"; }
+
     }
 
+    /// <summary> H:ストレッチ,V:Bottom,Pivot:底辺中央 </summary>
     public class RecTHorStretchBottom : RecTController {
 
         public float Left   { get { return OffmX; } set { OffmX = value; } }
@@ -43,6 +46,44 @@ namespace du.Cmp.RecT {
             PosY   = posY  ; Height = height;
         }
 
+    }
+
+    /// <summary> 右上原点 </summary>
+    public class RecTRightTop : RecTController {
+
+        /// <value> 右上頂点のX座標 </value>
+        public float PosX   { get { return OffMX; } set { OffMX = value; } }
+        /// <value> 右上頂点のY座標 </value>
+        public float PosY   { get { return OffMY; } set { OffMY = value; } }
+        public float Width  { get { return OffMX - OffmX; } set { OffmX = OffMX - value; } }
+        public float Height { get { return OffMY - OffmY; } set { OffmY = OffMY - value; } }
+
+        /// <summary> Transform親子関係の設定も合わせて行う </summary>
+        public RecTRightTop(RectTransform rect, Transform parent) : base(rect) {
+            Initialize(parent);
+        }
+        /// <summary> 事前に親設定済みの場合に使用 </summary>
+        public RecTRightTop(RectTransform rect) : base(rect) {
+            Initialize();
+        }
+
+        public void Initialize() {
+            if (RecT.parent == null) { Debug.LogError("ParentIsNull!!"); }
+            RecT.localScale = Vector3.one;
+            RecT.SetPivot(Pivot.TopRight);
+            RecT.SetAnchor(Anchor.TopRight);
+            RecT.position = RecT.position.ReZ(0);
+        }
+        public void Initialize(Transform parent) {
+            RecT.SetParent(parent);
+            Initialize();
+        }
+        public void Set(float posX, float posY, float width, float height) {
+            PosX = posX; Width  = width ;
+            PosY = posY; Height = height;
+        }
+
+        public override string ToString() { return $"RT[Pos({PosX}, {PosY}), Size({Width}, {Height})]"; }
     }
 
 }
