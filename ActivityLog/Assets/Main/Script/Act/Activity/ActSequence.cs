282350e5458e1d39cd0410fbe9304b8c16b181a2
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-06-07 23:19:30 +0900

    ActivitiesContainer -> ActSequence

---
+++ ActivityLog/Assets/Main/Script/Act/Activity/ActSequence.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Main.Act {
+
+    /// <summary> アクティビティの系列(日毎) </summary>
+    public interface IROActSequence {
+
+        /// <summary> 時系列順で index 番目 </summary>
+        /// <param name="index"> index 番目が存在しない場合例外を投げる </param>
+        IActivity this [int index] { get; }
+
+        /// <summary> 指定時刻に行われていたアクティビティ </summary>
+        IROActivity this [MinuteOfDay time] { get; }
+
+        /// <summary> 指定時刻に行われていたアクティビティの index </summary>
+        int IndexOf(MinuteOfDay time);
+
+        /// <summary> アクティビティ数 </summary>
+        int Count { get; }
+
+        /// <summary> 末尾のアクティビティ </summary>
+        IROActivity Back { get; }
+
+    }
+    public interface IActSequence : IROActSequence {
+
+        /// <summary> 末尾に追加 </summary>
+        void PushBack(IActivity act);
+
+        /// <summary>
+        /// 指定した時間に挿入
+        /// 完全に包含されるアクティビティは消滅
+        /// </summary>
+        void Insert(MinuteOfDay begin, MinuteOfDay end, IROContent cnt);
+
+        /// <summary> 直前のアクティビティが伸びる </summary>
+        void RemoveAt(int index);
+
+        /// <summary> 登録済みのアクティビティを移動させる </summary>
+        /// <param name="index"> 変更対象の index </param>
+        void Move(int index, MinuteOfDay newBegin, MinuteOfDay? newEnd);
+
+        /// <summary> 登録済みのアクティビティの内容を書き換える </summary>
+        void OverwriteCnt(int index, IROContent newContent);
+        /// <summary>
+        /// 登録済みのアクティビティの時間を前に伸ばす
+        /// 飲み込まれたアクティビティは消滅
+        /// </summary>
+        void OverwriteBeginTime(int index, MinuteOfDay newBegin);
+        void OverwriteEndTime(int index, MinuteOfDay? newEnd);
+
+    }
+
+}
---
+++ ActivityLog/Assets/Main/Script/Act/Activity/ActSequenceImpl.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Main.Act {
+
+    /// <summary> アクティビティの系列(日毎) </summary>
+    public class ActSequence : IActSequence {
+        #region field
+        IList<IActivity> m_acts;
+        #endregion
+
+        #region ctor/dtor
+        public ActSequence() { m_acts = new List<IActivity>(); }
+        #endregion
+
+        // IROActivitiesContainer -------
+        #region getter
+        public IActivity this [int index] { get { return m_acts[index]; } }
+        public IROActivity this [MinuteOfDay time] { get { return m_acts[IndexOf(time)]; } }
+        public int IndexOf(MinuteOfDay time) {
+            if (MinuteOfDay.Now < time) {
+                throw new System.ArgumentException("Argument is future.");
+            }
+            else {
+                for (int i = m_acts.Count - 1; i >= 0; --i) {
+                    if (m_acts[i].Context.BeginTime < time) {
+                        return i;
+                    }
+                }
+            }
+            // ここには本来到達し得ない
+            throw new System.Exception("");
+        }
+
+        public int Count { get { return m_acts.Count; } }
+        public IROActivity Back { get { return m_acts[m_acts.Count - 1]; } }
+        private IActivity MutableBack { get { return m_acts[m_acts.Count - 1]; } }
+        private bool IndexIsValid(int index) { return 0 <= index && index < m_acts.Count; }
+        #endregion
+        // ------------------------------
+
+        #region public
+        public void PushBack(IActivity act) {
+            if (m_acts.Count == 0) {
+                m_acts.Add(act);
+            }
+            else if (Back.Context.BeginTime < act.Context.BeginTime) {
+                Debug.LogError($"PushBack[{Back.Context.BeginTime} >< {act.Context.BeginTime}]");
+                MutableBack.MutableContext.ResetFollowAct(act);
+                m_acts.Add(act);
+            }
+            else {
+                Debug.LogError($"PushBack[{Back.Context.BeginTime} >< {act.Context.BeginTime}]");
+                for (int i = Count - 1; i > IndexOf(act.Context.BeginTime); --i) {
+                    RemoveAt(i);
+                }
+                PushBack(act);
+            }
+        }
+        public void Insert(MinuteOfDay begin, MinuteOfDay end, IROContent cnt) {
+            IIndependentActivity act = new IndependentActivity(cnt, new IndependentContext(begin, end));
+            int justBefore = IndexOf(act.IndependentContext.BeginTime);
+            int justAfter = IndexOf(act.IndependentContext.EndTime);
+            // e.g.        |---- act ----|
+            // |-- justBefore --|- In -|-- justAfter --| の場合
+
+            // (1)包含アクティビティを削除
+            //             |---- act ----|
+            // |-- justBefore ---------|-- justAfter --|
+            for (int i = justAfter - 1; i > justBefore; --i) {
+                RemoveAt(i); // 後ろから消さないとindexがずれる
+            }
+            justAfter = justBefore + 1;
+            // (2)挿入位置の直後のアクティビティ -> Precede を書き換え
+            //             |---- act ----|
+            // |-- jB -------------------|----- jA ----|
+            m_acts[justAfter].MutableContext.ResetPrecedeAct(act);
+            // (3)MakeDependしてコンテナへ
+            //             |---- act ----|----- jA ----|
+            // |-- jB -------------------|----- jA ----|
+            m_acts.Insert(justAfter, act.MakeDepend(m_acts[justAfter]));
+            // (4)挿入位置の直前のアクティビティ -> Follow を書き換え
+            // |-- jB -----|---- act ----|----- jA ----|
+            m_acts[justBefore].MutableContext.ResetFollowAct(m_acts[justAfter]);
+        }
+        public void RemoveAt(int index) {
+            // 無効な index : 削除自体しない
+            if (!IndexIsValid(index)) { return; }
+            // 先頭以外 : 直前のアクティビティを直後の開始時刻まで延長
+            // > 末尾 : 直前のアクティビティを再開
+            else if (index > 0) {
+                m_acts[index - 1].MutableContext.ResetFollowAct(m_acts[index].MutableContext.NextAct);
+            }
+            // 先頭 : 直後のアクティビティの開始時刻を00:00に伸ばす
+            else {
+                m_acts[index + 1].MutableContext.ResetPrecedeAct(MinuteOfDay.Begin);
+            }
+            m_acts.RemoveAt(index);
+        }
+        public void Move(int index, MinuteOfDay newBegin, MinuteOfDay? newEnd) {
+            var act = m_acts[index];
+            RemoveAt(index);
+            if (newEnd != null) { Insert(newBegin, (MinuteOfDay)newEnd, act.Content); }
+            else { PushBack(act); }
+        }
+        public void OverwriteCnt(int index, IROContent newContent) {
+            m_acts[index].ResetContent(newContent);
+        }
+        public void OverwriteBeginTime(int index, MinuteOfDay newBegin) {
+            Move(index, newBegin, m_acts[index].Context.EndTime);
+        }
+        public void OverwriteEndTime(int index, MinuteOfDay? newEnd) {
+            Move(index, m_acts[index].Context.BeginTime, newEnd);
+        }
+        #endregion
+    }
+
+}
