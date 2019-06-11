5a287d5ea611a623d95cffb2671cb934c89d8e2c
direct1889 <s969.6261asthastnkzgfk@gmail.com>
2019-06-11 16:10:12 +0900

    コード整形、AddAct()をPublish

--- ActivityLog/Assets/Main/Script/Act/DB/ContentDB.cs
+++ ActivityLog/Assets/Main/Script/Act/DB/ContentDB.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UColor = UnityEngine.Color;
 using System.Linq;
 using static Main.Act.DB.ExProject;
+using System;
+using UniRx;
 
 namespace Main.Act.DB {
 
     public static class ContentDB {
-        #region property
+        #region field
         public static IProjectDB Proj { get; } = new ProjectDB();
         public static IActivityDB Act { get; } = new ActivityDB();
         #endregion
@@ -16,23 +18,29 @@ namespace Main.Act.DB {
         #region public
         public static void Initialize() {
             du.Test.LLog.MBoot.Log("Initialized ContentDB.");
-            Proj.Initialize();  // 必ずProjを先に初期化
-            Act.Initialize();   // Actの初期化時Projにアクセスする
+            Proj.Initialize(); // 必ずProjを先に初期化
+            Act .Initialize(); // Actの初期化時Projにアクセスする
         }
         #endregion
     }
 
     public interface IActivityDB : du.Cmp.IOrderedMap<IROContent> {
         //! 登録済みActivity一覧の生成
+        /// <summary> 登録済みActivity一覧の生成 </summary>
         void Initialize();
         /// <summary> ActivityをEnumerableで一括取得 </summary>
         IEnumerable<IROContent> Sorted(IProject parent);
+        // TODO: OVERLAP
+        /// <summary> 既存アクティビティと重複するか </summary>
+        // bool HasExistOverlapped(string name, IProject parent);
+        /// <summary> Activityを新たに登録 </summary>
+        void AddAct(IROContent content);
     }
 
     /// <summary> 全てのプロジェクトは事前にDBに登録が必要 </summary>
     public class ActivityDB : du.Cmp.OrderedMap<IROContent>, IActivityDB {
         #region public
-        //! 登録済みActivity一覧の生成
+        /// <summary> 登録済みのActivityをCSVから生成 </summary>
         public void Initialize() { Load("System/Activities"); }
 
         /// <summary> 指定したProjectを直属の親に持つActivityのみを取得 </summary>
@@ -41,10 +49,17 @@ namespace Main.Act.DB {
                 .Where(key => At(key).Parent == parent)
                 .Select(key => At(key));
         }
+        // TODO: OVERLAP
+        // public bool HasExistOverlapped(string name, IProject parent) {
+        //     if (!Data.ContainsKey(name)) { return false; }
+        //     var sameNames = Data.Where(act => act.Key == name);
+        // }
+
+        /// <summary> Activityを新たに登録 </summary>
+        public void AddAct(IROContent content) { Add(content.Name, content); }
         #endregion
 
         #region private
-        private void AddAct(IROContent content) { Add(content.Name, content); }
         private void AddAct(string parentName, string name                  ) { Add(name, new Content(ContentDB.Proj.At(parentName), name             )); }
         private void AddAct(string parentName, string name, bool isEffective) { Add(name, new Content(ContentDB.Proj.At(parentName), name, isEffective)); }
 
@@ -70,9 +85,7 @@ namespace Main.Act.DB {
         #region public
         //! 登録済みProject一覧の生成
         public void Initialize() { Load("System/Projects"); }
-        #endregion
 
-        #region public
         /// <summary> 指定したProjectを直属の親に持つActivityのみを取得 </summary>
         public IEnumerable<IProject> Sorted(IProject parent) {
             return Order
